0fc2b438359901e3b2cc915283f6fe269d1fb4eb
sim <lava@shinespark.io>
2017-10-25 16:58:51 -0500

    further functions from CncrList rounded out

--- nanovg_test/nanovg_test/flat_lockfree_map.hpp
+++ nanovg_test/nanovg_test/flat_lockfree_map.hpp
@@ -23,122 +23,6 @@
 
 struct flf_map
 {
-  class     CncrLst
-  {
-    // Copied from simdb 2017.10.25
-    // Internally this is an array of indices that makes a linked list
-    // Externally indices can be gotten atomically and given back atomically
-    // | This is used to get free indices one at a time, and give back in-use indices one at a time
-    // Uses the first 8 bytes that would normally store sizeBytes as the 64 bits of memory for the Head structure
-    // Aligns the head on a 64 bytes boundary with the rest of the memory on a separate 64 byte boudary. This puts them on separate cache lines which should eliminate false sharing between cores when atomicallyaccessing the Head union (which will happen quite a bit) 
-  public:
-    //using     u32  =  uint32_t;
-    //using     u64  =  uint64_t;
-    //using    au64  =  std::atomic<u64>;
-    using ListVec  =  lava_vec<u32>;
-
-    //union Head
-    //{
-    //  struct { u32 ver; u32 idx; };                           // ver is version, idx is index
-    //  u64 asInt;
-    //};
-    union Head
-    {
-      u8  dblCachePad[128];
-      u32         idx;                                         // idx is index
-    };
-
-
-    static const u32        LIST_END = 0xFFFFFFFF;
-    static const u32 NXT_VER_SPECIAL = 0xFFFFFFFF;
-
-  private:
-    //ListVec     s_lv;
-    //au64*        s_h;
-
-  public:
-    //static u64   sizeBytes(u32 size) { return ListVec::sizeBytes(size) + 128; }         // an extra 128 bytes so that Head can be placed
-    static u64   sizeBytes(u32 size) { return sizeof(u32) + 128; }         // an extra 128 bytes so that Head can be placed
-    static u32  incVersion(u32    v) { return v==NXT_VER_SPECIAL?  1  :  v+1; }
-
-    CncrLst(){}
-    CncrLst(void* addr, void* headAddr, u32 size, bool owner=true)             // this constructor is for when the memory is owned an needs to be initialized
-    {                                                                          // separate out initialization and let it be done explicitly in the simdb constructor?    
-      u64    addrRem  =  (u64)headAddr % 64;
-      u64  alignAddr  =  (u64)headAddr + (64-addrRem);
-      assert( alignAddr % 64 == 0 );
-      au64*       hd  =  (au64*)alignAddr;
-      u32*   lstAddr  =  (u32*)((u64)alignAddr+64);
-      //new (&s_lv) ListVec(listAddr, size, owner);
-      
-      // give the list initial values here
-      //u32* lstAddr = 
-      //if(owner){
-      for(u32 i=0; i<(size-1); ++i){ lstAddr[i] = i+1; }
-      lstAddr[size-1] = LIST_END;
-
-      //((Head*)s_h)->idx = 0;
-      //((Head*)s_h)->ver = 0;
-      //}
-    }
-
-    u32         nxt()                                                             // moves forward in the list and return the previous index
-    {
-      Head  curHead, nxtHead;
-      curHead.asInt  =  s_h->load();
-      do{
-        if(curHead.idx==LIST_END){return LIST_END;}
-
-        nxtHead.idx  =  s_lv[curHead.idx];
-        nxtHead.ver  =  curHead.ver==NXT_VER_SPECIAL? 1  :  curHead.ver+1;
-      }while( !s_h->compare_exchange_strong(curHead.asInt, nxtHead.asInt) );
-
-      return curHead.idx;
-    }
-    u32        free(u32 idx)                                                    // not thread safe when reading from the list, but it doesn't matter because you shouldn't be reading while freeing anyway, since the CncrHsh will already have the index taken out and the free will only be triggered after the last reader has read from it 
-    {
-      Head curHead, nxtHead; u32 retIdx;
-      curHead.asInt = s_h->load();
-      do{
-        retIdx = s_lv[idx] = curHead.idx;
-        nxtHead.idx  =  idx;
-        nxtHead.ver  =  curHead.ver + 1;
-      }while( !s_h->compare_exchange_strong(curHead.asInt, nxtHead.asInt) );
-
-      return retIdx;
-    }
-    u32        free(u32 st, u32 en)                                            // not thread safe when reading from the list, but it doesn't matter because you shouldn't be reading while freeing anyway, since the CncrHsh will already have the index taken out and the free will only be triggered after the last reader has read from it 
-    {
-      Head curHead, nxtHead; u32 retIdx;
-      curHead.asInt = s_h->load();
-      do{
-        retIdx = s_lv[en] = curHead.idx;
-        nxtHead.idx  =  st;
-        nxtHead.ver  =  curHead.ver + 1;
-      }while( !s_h->compare_exchange_strong(curHead.asInt, nxtHead.asInt) );
-
-      return retIdx;
-    }
-    auto      count() const -> u32 { return ((Head*)s_h)->ver; }
-    auto        idx() const -> u32
-    {
-      Head h; 
-      h.asInt = s_h->load();
-      return h.idx;
-    }
-    auto       list() -> ListVec const* { return &s_lv; }                      // not thread safe
-    u32      lnkCnt()                                                          // not thread safe
-    {
-      u32    cnt = 0;
-      u32 curIdx = idx();
-      while( curIdx != LIST_END ){
-        curIdx = s_lv[curIdx];
-        ++cnt;
-      }
-      return cnt;
-    }
-    auto       head() -> Head* { return (Head*)s_h; }
-  };
 
   using   u8    =    uint8_t;
   using  u32    =   uint32_t;
@@ -164,6 +48,11 @@ struct flf_map
     struct { Idx first; Idx second; };
     u64 asInt;
   };
+  union    LstHead
+  {
+    u32   idx;
+    u32 count;
+  };
   struct    Header {
     // first 8 bytes - two 1 bytes characters that should equal 'lm' for lockless map
     u64     typeChar1  :  8;
@@ -171,17 +60,18 @@ struct flf_map
     u64     sizeBytes  : 48;
     
     // next 8 bytes keep track of the size of the values and number inserted - from that and the sizeBytes, capacity can be inferred
-    u64          size :  32;      // this could be only 24 bits since that is the max index
+    u64          size :  32;                                    // this could be only 24 bits since that is the max index
     u64  valSizeBytes :  32;
 
     // next 4 bytes is the current block list
-    u32        curBlk;
+    u32         lstHd;                                          // lstHd is list head
   };
   struct   BlkMeta {
   };
 
   u8*     m_mem = nullptr;  // single pointer is all that ends up on the stack
 
+  // concurrent list functions
   void   make_list(void* addr, u32* head, u32 size)               // this constructor is for when the memory is owned an needs to be initialized
   {                                                             // separate out initialization and let it be done explicitly in the simdb constructor?    
     u32*  lstAddr  =  (u32*)addr;
@@ -221,6 +111,8 @@ struct flf_map
 
     return retIdx;
   }
+  u32          idx(au32* head) const { return head->load(); }
+  u32*       head(){ return &(((Header*)m_mem)->lstHd); }
   //u32        count(au32* head) const { return ((Header*)head)->cap; }
 
   u64   slotByteOffset(u64 idx){ return sizeof(Header) + idx*sizeof(IdxPair); }
@@ -287,6 +179,136 @@ struct flf_map
 
 
 
+
+
+
+//class     CncrLst
+//{
+//  // Copied from simdb 2017.10.25
+//  // Internally this is an array of indices that makes a linked list
+//  // Externally indices can be gotten atomically and given back atomically
+//  // | This is used to get free indices one at a time, and give back in-use indices one at a time
+//  // Uses the first 8 bytes that would normally store sizeBytes as the 64 bits of memory for the Head structure
+//  // Aligns the head on a 64 bytes boundary with the rest of the memory on a separate 64 byte boudary. This puts them on separate cache lines which should eliminate false sharing between cores when atomicallyaccessing the Head union (which will happen quite a bit) 
+//public:
+//  //using     u32  =  uint32_t;
+//  //using     u64  =  uint64_t;
+//  //using    au64  =  std::atomic<u64>;
+//  using ListVec  =  lava_vec<u32>;
+//
+//  //union Head
+//  //{
+//  //  struct { u32 ver; u32 idx; };                           // ver is version, idx is index
+//  //  u64 asInt;
+//  //};
+//  union Head
+//  {
+//    u8  dblCachePad[128];
+//    u32         idx;                                         // idx is index
+//  };
+//
+//  static const u32        LIST_END = 0xFFFFFFFF;
+//  static const u32 NXT_VER_SPECIAL = 0xFFFFFFFF;
+//
+//private:
+//  //ListVec     s_lv;
+//  //au64*        s_h;
+//
+//public:
+//  //static u64   sizeBytes(u32 size) { return ListVec::sizeBytes(size) + 128; }         // an extra 128 bytes so that Head can be placed
+//  static u64   sizeBytes(u32 size) { return sizeof(u32) + 128; }         // an extra 128 bytes so that Head can be placed
+//  static u32  incVersion(u32    v) { return v==NXT_VER_SPECIAL?  1  :  v+1; }
+//
+//  CncrLst(){}
+//  CncrLst(void* addr, void* headAddr, u32 size, bool owner=true)             // this constructor is for when the memory is owned an needs to be initialized
+//  {                                                                          // separate out initialization and let it be done explicitly in the simdb constructor?    
+//    u64    addrRem  =  (u64)headAddr % 64;
+//    u64  alignAddr  =  (u64)headAddr + (64-addrRem);
+//    assert( alignAddr % 64 == 0 );
+//    au64*       hd  =  (au64*)alignAddr;
+//    u32*   lstAddr  =  (u32*)((u64)alignAddr+64);
+//    //new (&s_lv) ListVec(listAddr, size, owner);
+//    
+//    // give the list initial values here
+//    //u32* lstAddr = 
+//    //if(owner){
+//    for(u32 i=0; i<(size-1); ++i){ lstAddr[i] = i+1; }
+//    lstAddr[size-1] = LIST_END;
+//
+//    //((Head*)s_h)->idx = 0;
+//    //((Head*)s_h)->ver = 0;
+//    //}
+//  }
+//
+//  u32         nxt()                                                             // moves forward in the list and return the previous index
+//  {
+//    Head  curHead, nxtHead;
+//    curHead.asInt  =  s_h->load();
+//    do{
+//      if(curHead.idx==LIST_END){return LIST_END;}
+//
+//      nxtHead.idx  =  s_lv[curHead.idx];
+//      nxtHead.ver  =  curHead.ver==NXT_VER_SPECIAL? 1  :  curHead.ver+1;
+//    }while( !s_h->compare_exchange_strong(curHead.asInt, nxtHead.asInt) );
+//
+//    return curHead.idx;
+//  }
+//  u32        free(u32 idx)                                                    // not thread safe when reading from the list, but it doesn't matter because you shouldn't be reading while freeing anyway, since the CncrHsh will already have the index taken out and the free will only be triggered after the last reader has read from it 
+//  {
+//    Head curHead, nxtHead; u32 retIdx;
+//    curHead.asInt = s_h->load();
+//    do{
+//      retIdx = s_lv[idx] = curHead.idx;
+//      nxtHead.idx  =  idx;
+//      nxtHead.ver  =  curHead.ver + 1;
+//    }while( !s_h->compare_exchange_strong(curHead.asInt, nxtHead.asInt) );
+//
+//    return retIdx;
+//  }
+//  u32        free(u32 st, u32 en)                                            // not thread safe when reading from the list, but it doesn't matter because you shouldn't be reading while freeing anyway, since the CncrHsh will already have the index taken out and the free will only be triggered after the last reader has read from it 
+//  {
+//    Head curHead, nxtHead; u32 retIdx;
+//    curHead.asInt = s_h->load();
+//    do{
+//      retIdx = s_lv[en] = curHead.idx;
+//      nxtHead.idx  =  st;
+//      nxtHead.ver  =  curHead.ver + 1;
+//    }while( !s_h->compare_exchange_strong(curHead.asInt, nxtHead.asInt) );
+//
+//    return retIdx;
+//  }
+//  auto      count() const -> u32 { return ((Head*)s_h)->ver; }
+//  auto        idx() const -> u32
+//  {
+//    Head h; 
+//    h.asInt = s_h->load();
+//    return h.idx;
+//  }
+//  auto       list() -> ListVec const* { return &s_lv; }                      // not thread safe
+//  u32      lnkCnt()                                                          // not thread safe
+//  {
+//    u32    cnt = 0;
+//    u32 curIdx = idx();
+//    while( curIdx != LIST_END ){
+//      curIdx = s_lv[curIdx];
+//      ++cnt;
+//    }
+//    return cnt;
+//  }
+//  auto       head() -> Head* { return (Head*)s_h; }
+//};
+
+//auto      list() -> ListVec const* { return &s_lv; }                      // not thread safe
+//u32     lnkCnt()                                                           // not thread safe
+//{
+//  u32    cnt = 0;
+//  u32 curIdx = idx();
+//  while( curIdx != LIST_END ){
+//    curIdx = s_lv[curIdx];
+//    ++cnt;
+//  }
+//  return cnt;
+//}
 
 //
 //nxtHead.ver  =  curHead.ver + 1;
